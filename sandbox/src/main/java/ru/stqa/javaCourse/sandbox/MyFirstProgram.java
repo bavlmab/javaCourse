0ff584a17982b2fcc6f8265f5d0f54b956456748
bavlmab <80824518+bavlmab@users.noreply.github.com>
2021-03-18 14:02:54 +0300

    Create methods

--- sandbox/src/main/java/ru/stqa/javaCourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javaCourse/sandbox/MyFirstProgram.java
@@ -12,19 +12,12 @@ public class MyFirstProgram {
         /*r.a = 4;
         r.b = 6;*/
 
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");;
     }
 
-	public static double area(Square s){
-	    return s.l * s.l;
-    }
-
-    public static double area(Rectangle r){
-        return r.a * r.b;
-    }
 }
