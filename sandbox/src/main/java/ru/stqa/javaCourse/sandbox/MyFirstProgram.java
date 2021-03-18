5a1b7c69edcf2c1fa41d1e3c52fc7161b31f9ad1
bavlmab <80824518+bavlmab@users.noreply.github.com>
2021-03-18 10:45:38 +0300

    Added several functions

--- sandbox/src/main/java/ru/stqa/javaCourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javaCourse/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package ru.stqa.javaCourse.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		hello("Daria");
+
+		double l = 5;
+		double a = 4;
+		double b = 6;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 	}
 
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");;
+    }
+
+	public static double area(double len){
+	    return len * len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
+    }
 }
