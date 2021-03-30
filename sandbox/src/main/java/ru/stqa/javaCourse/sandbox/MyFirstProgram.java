0503686813d7dd4db484b979315a70e4943c0a8f
bavlmab <80824518+bavlmab@users.noreply.github.com>
2021-03-30 20:40:38 +0300

    Creat first test and use TestNG

--- sandbox/src/main/java/ru/stqa/javaCourse/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javaCourse/sandbox/MyFirstProgram.java
@@ -6,11 +6,8 @@ public class MyFirstProgram {
 		hello("Daria");
 
 		Square s = new Square(5);
-        //s.l = 5; Атрибуты заполняются в конструкторе
 
         Rectangle r = new Rectangle(4, 6);
-        /*r.a = 4;
-        r.b = 6;*/
 
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
